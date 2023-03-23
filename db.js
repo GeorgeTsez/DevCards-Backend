e1d8107366fbbbf54b8e2eb0c9be05a1f4a0c14f
reemhd <reemhde@gmail.com>
2023-03-23 16:21:22 +0000

    intial commit

--- db.js
+++ db.js
@@ -2,22 +2,58 @@ const mongoose = require("mongoose");
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose
   .connect(
-    "mongodb+srv://Cluster72529:T9xm2L7l1D8k5Ld1@cluster72529.8kzzf3d.mongodb.net/test",options
+    "mongodb+srv://Cluster64244:bIkmux@cluster64244.fhbnizy.mongodb.net/?retryWrites=true&w=majority",
+    options
   )
   .then(() => {
-    console.log("connected");
+    console.log("Connected to MongoDB Atlas");
   })
   .catch((err) => {
     console.log(err);
   });
 
-const userSchema = new mongoose.Schema({name: String, age: Number})
-const User = mongoose.model("User",userSchema)
-const newUser = new User({name:"Reem", age: 18})
-newUser.save()
-.then((result)=>{
-    console.log(result)
+const userSchema = new mongoose.Schema({ name: String, age: Number });
+
+const User = mongoose.model("User", userSchema);
+
+const newUser = new User({ name: "Reem", age: 18 });
+
+newUser
+  .save()
+  .then((result) => {
+    // console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+User.findOne({name: 'Reem' }, 'name')
+.then(result => {
+  // console.log(result)
 })
-.catch((err)=>{
-    console.log(err)
+.catch(err => {
+  console.log(err)
 })
+
+User.insertMany([
+  { name: "Matt"},
+  { name: "Rogerio" },
+  { name: "Danny" },
+  { name: "Georgios" },
+])
+.then(result => {
+  // console.log(result)
+})
+.catch(err => console.log(err))
+
+
+User.updateOne({name: 'Reem'}, {age: 15})
+.then(result => {
+  // console.log(result)
+})
+.catch(err => console.log(err))
+
+User.find()
+.then(users => console.log(users))
+.catch(err => console.log(err))
