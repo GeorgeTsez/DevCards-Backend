b1d01c28cec8e654c60d6b7246e8b7a3f96305b6
Georgios tsesmelis <georgiostsesmelis@192.168.1.102>
2023-03-27 12:24:53 +0100

    seeded

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,26 +1,50 @@
 const connection = require("../../connection");
 const mongoose = require("mongoose");
-const testdata = require("../data/testdata/testcards.json");
-const testdecks = require("../data/testdata/testdecks.json")
+const testCards = require("../data/testdata/testcards.json");
+const testdecks = require("../data/testdata/testdecks.json");
+const testUsers = require("../data/testdata/testUsers.json")
 
 connection
   .then(() => {
     const cardSchema = new mongoose.Schema({ front: String, back: String });
-    const deckSchema = new mongoose.Schema({title: String, description: String, cards:Object})
+    const deckSchema = new mongoose.Schema({
+      title: String,
+      description: String,
+      cards: Object,
+    })
+    const userSchema = new mongoose.Schema({username: String, email: String, user_decks: Object});
     const Card = mongoose.model("Card", cardSchema);
-    const Deck = mongoose.model("Deck", deckSchema)
-    Promise.all([ Card.insertMany(testdata), Deck.insertMany(testdecks)])
-      .then(([cardsFromDb, decksFromDb]) => {
-        Promise.all([...cardsFromDb.map((card, index) => {
-          if (index < 7) return Deck.updateOne({_id: decksFromDb[0]._id}, {$addToSet: {cards: card._id}})
-          if (index === 7) return Deck.updateOne({_id: decksFromDb[1]._id}, {$addToSet: {cards: card._id}})
-          if (index === 8) return Deck.updateOne({_id: decksFromDb[3]._id}, {$addToSet: {cards: card._id}})
-          if (index > 8) return Deck.updateOne({_id: decksFromDb[4]._id}, {$addToSet: {cards: card._id}})
-        })])
-        .then((resp) => {
-          console.log(resp)
+    const Deck = mongoose.model("Deck", deckSchema);
+    const User = mongoose.model("User", userSchema)
+    Promise.all([Card.insertMany(testCards), Deck.insertMany(testdecks), User.insertMany(testUsers)])
+      .then(([cardsFromDb, decksFromDb, usersFromDb]) => {
+        Promise.all([
+          ...cardsFromDb.map((card, index) => {
+            if (index < 7)
+              return Deck.updateOne(
+                { _id: decksFromDb[0]._id },
+                { $addToSet: { cards: card._id } }
+              );
+            if (index === 7)
+              return Deck.updateOne(
+                { _id: decksFromDb[1]._id },
+                { $addToSet: { cards: card._id } }
+              );
+            if (index === 8)
+              return Deck.updateOne(
+                { _id: decksFromDb[3]._id },
+                { $addToSet: { cards: card._id } }
+              );
+            if (index > 8)
+              return Deck.updateOne(
+                { _id: decksFromDb[4]._id },
+                { $addToSet: { cards: card._id } }
+              );
+          }), User.insertOne({_id: usersFromDb[0]})
+        ]).then((resp) => {
+          console.log(resp);
           mongoose.connection.close();
-        })
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -30,4 +54,3 @@ connection
   .catch((err) => {
     console.log(err);
   });
-
