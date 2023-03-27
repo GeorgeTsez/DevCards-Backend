e24c2889c3cbebb0f49c5757248f1ef9a5dbb6cc
Rogerio Soares <rogeryu_gap@hotmail.com>
2023-03-27 14:45:54 +0100

    Organised Schemas and Models updated the git ignore

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,22 +1,13 @@
-const connection = require("../../connection");
-const mongoose = require("mongoose");
+const {Card, Deck, User} = require("../models/models")
+const connection = require("../connection");
+const mongoose = require('mongoose')
 const testCards = require("../data/testdata/testcards.json");
-const testdecks = require("../data/testdata/testdecks.json");
+const testDecks = require("../data/testdata/testdecks.json");
 const testUsers = require("../data/testdata/testUsers.json")
 
 connection
   .then(() => {
-    const cardSchema = new mongoose.Schema({ front: String, back: String });
-    const deckSchema = new mongoose.Schema({
-      title: String,
-      description: String,
-      cards: Object,
-    })
-    const userSchema = new mongoose.Schema({username: String, email: String, user_decks: Object});
-    const Card = mongoose.model("Card", cardSchema);
-    const Deck = mongoose.model("Deck", deckSchema);
-    const User = mongoose.model("User", userSchema)
-    Promise.all([Card.insertMany(testCards), Deck.insertMany(testdecks), User.insertMany(testUsers)])
+    Promise.all([Card.insertMany(testCards), Deck.insertMany(testDecks), User.insertMany(testUsers)])
       .then(([cardsFromDb, decksFromDb, usersFromDb]) => {
         Promise.all([
           ...cardsFromDb.map((card, index) => {
@@ -40,9 +31,11 @@ connection
                 { _id: decksFromDb[4]._id },
                 { $addToSet: { cards: card._id } }
               );
-          }), User.insertOne({_id: usersFromDb[0]})
+          }),
+          User.updateOne({_id: usersFromDb[0]}, 
+            {$addToSet: { user_decks: [...decksFromDb.map((deck) => {return deck._id})]} 
+          })
         ]).then((resp) => {
-          console.log(resp);
           mongoose.connection.close();
         });
       })
