2ef6719e98625a8bd2e2b3446e617a32710f3f89
Nanobot1514 <mjbishop1514@gmail.com>
2023-03-29 14:45:29 +0100

    possible fix for db connection in decks-model

--- models/decks-models.js
+++ models/decks-models.js
@@ -2,7 +2,9 @@ const db = require("../db/connection");
 const { Deck } = require("../db/models/decks");
 
 exports.fetchDecks = () => {
-  return Deck.find({}).then((decks) => {
-    return decks;
+  db.then(() => {
+    return Deck.find({}).then((decks) => {
+      return decks;
+    });
   });
 };
