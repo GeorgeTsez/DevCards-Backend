8eb2889071ec46ba243cf9f0c7dd8ac666c37cc1
Nanobot1514 <mjbishop1514@gmail.com>
2023-03-29 15:07:21 +0100

    change to query to open connection

--- models/decks-models.js
+++ models/decks-models.js
@@ -1,10 +1,9 @@
-const db = require("../db/connection");
+const connection = require("../db/connection");
 const { Deck } = require("../db/models/decks");
 
 exports.fetchDecks = () => {
-  db.then(() => {
-    return Deck.find({}).then((decks) => {
-      return decks;
-    });
+  connection();
+  return Deck.find({}).then((decks) => {
+    return decks;
   });
 };
