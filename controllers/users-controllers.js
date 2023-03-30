be49da68e4e7b50a6ae72de562684558825e31e2
Rogerio Soares <rogeryu_gap@hotmail.com>
2023-03-30 14:04:42 +0100

    added dynamic id capability

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -2,7 +2,6 @@ const { fetchUserById } = require("../models/user-models");
 
 exports.getUser = (req, res, next) => {
   const id = req.params.user_id
-  console.log(id)
   fetchUserById(id)
     .then((user) => {
       res.status(200).send({ user });
--- models/user-models.js
+++ models/user-models.js
@@ -1,6 +1,5 @@
 const { User } = require("../db/models/users");
 
 exports.fetchUserById = async (id) => {
-
   return User.find({_id: id});
 };
