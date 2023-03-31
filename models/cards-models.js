e9b3e0132fdf63f0993dbd0b5848fd9b52c7745a
Georgios tsesmelis <georgiostsesmelis@192.168.1.102>
2023-03-31 12:39:38 +0100

    removed deck from create single card

--- models/cards-models.js
+++ models/cards-models.js
@@ -3,7 +3,7 @@ const { Deck } = require("../db/models/decks");
 
 exports.createSingleCard = async (body, deckId) => {
   return Card.create(body).then((card) => {
-    return Deck.findByIdAndUpdate(deckId, { $addToSet: { cards: card._id} }).then((deck)=>{
+    return Deck.findByIdAndUpdate(deckId, { $addToSet: { cards: card._id} }).then(()=>{
         return card
     });
   });
