6f8bdf1785c66f3ffad2743712ca74d4e7e00bc4
Nanobot1514 <mjbishop1514@gmail.com>
2023-03-31 16:25:59 +0100

    Remove console.log

--- controllers/cards-controller.js
+++ controllers/cards-controller.js
@@ -2,7 +2,6 @@ const { removeCardById } = require("../models/cards-models");
 
 exports.deleteCardById = (req, res, next) => {
   const { card_id } = req.params;
-  console.log(card_id);
   removeCardById(card_id)
     .then(() => {
       res.sendStatus(204);
